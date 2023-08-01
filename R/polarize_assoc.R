d1b3519c83a8ed0586680590ab818bababa36261
davidtcaldwell <dtcaldwell13@gmail.com>
2023-07-31 17:39:23 -0700

    Adding documentation to poalrize_assoc function

--- R/polarize_assoc.R
+++ R/polarize_assoc.R
@@ -5,7 +5,7 @@
 #' @param data A data set object
 #' @param value_1 A column containing values on some variable or set of variables, such as attitude item responses.
 #' @param value_2 A column containing values on a second variable or set of variables (e.g., political party affiliation).
-#' @param r_or_r2 A string specifying the measure of association. Use "r" for the Pearson correlation between two numeric value columns. Use "r2" to return the coefficient of determination and adjusted \code{R^2} from OLS models predicting a numeric outcome from whatever is supplied to the value_2 argument, including categorical predictors (such as unordered multiparty values).
+#' @param r_or_r2 A string specifying the measure of association. See Details below.
 #' @param by A character vector of optional groups to nest observations by (e.g., survey wave, country, social group).
 #' @param ids Variables specifying cluster ids from largest level to smallest level (leaving the argument empty, NULL, 1, or 0 indicate no clusters).
 #' @param probs Variables specifying cluster sampling probabilities.
@@ -16,11 +16,25 @@
 #' @param check_strata If \code{TRUE}, check that clusters are nested in strata.
 #' @param weights Variables specifying weights (inverse of probability).
 #' @param pps "brewer" to use Brewer's approximation for PPS sampling without replacement. "overton" to use Overton's approximation. An object of class HR to use the Hartley-Rao approximation. An object of class ppsmat to use the Horvitz-Thompson estimator.
-#' @param variance For pps without replacement, use variance="YG" for the Yates-Grundy estimator instead of the Horvitz-Thompson estimator
+#' @param variance For pps without replacement, use variance="YG" for the Yates-Grundy estimator instead of the Horvitz-Thompson estimator.
 #'
-#' @return A data frame object containing the measure of association between two sets of values
+#' @return A data frame object containing the measure of association between two sets of values.
+#' 
+#' @details 
+#' This function implements two prominent approaches to estimating polarization as the association between two sets of values, such as attitudes and partisanship. In a mostly US-dominated literature, the established approach is to use the Pearson correlation between an ordinal or binary attitude item and an ordinal measure of partisanship (e.g., Baldassarri and Gelman 2008). This approach is implemented by setting the \code{r_or_r2} argument to "r", returing the Pearson correlation between two value columns of class \code{numeric}.
+#' 
+#' The second approach is to use the $R^2$ or adjusted $R^2$ from OLS model predicting values supplied to the \code{value_1} argument from values supplied to the \code{value_2} argument, including categorical predictors (such as unordered multiparty values). This approach is implemented by setting the \code{r_or_r2} argument to "r2", returning the $R^2$ and adjusted $R^2$ corresponding to square of the correlation between observed and predicted outcomes (see Caldwell, Cohen, and Vivyan 2023).
+#' 
+#' The function is useful when you want to calculate associational measures of polarization over multiple variables or groups, such as attitude items nested in survey waves. It is designed to work with the \code{survey} package, allowing the incorporation of complex survey design features. This was previously inconvenient when working with large numbers of attitude items simultaneously, as the \code{survey} package requires the user to specify the variable name in a formula. The function allows the user to specify variable names and any other grouping information in the \code{by} argument, which are used to nest attitude item responses and apply functions related to the \code{survey} package.
+#' 
+#' In the case where \code{r_or_r2} is set to "r2", the function will automatically fit an OLS regression model of class \code{svyglm} (see \code{\link[survey]{svyglm}} for more details). Values supplied to the \code{value_1} argument will be used as the dependent variable, and values supplied to the \code{value_2} argument will be used as the independent variable. The function will then extract the $R^2$ and adjusted $R^2$ from each model and return them in the output data frame.
+#' 
+#' When \code{r_or_r2} is set to "r", the function uses \code{jtools::svycor} to calculate the Pearson correlation between \code{value_1} and \code{value_2}. \code{jtools::svycor} is a wrapper around 
+#' 
+#' 
 #'
 #' @examples
+#' data(toydata)
 #' # Partisan polarization using Pearson correlation between attitude item and ordinal party affiliation variable
 #' party_pol <- polarize_assoc(data = toydata, value_1 = att_val, value_2 = party_val, r_or_r2 = "r", by = c("att_name", "group", "year"))
 #'
@@ -82,7 +96,7 @@ polarize_assoc <- function(
         !is.numeric(value_1_eval) | !is.numeric(value_2_eval)
         ) {
         stop(
-          "Pearson correlations innapropriate for unordered values."
+          "Pearson correlations require two value columns of class numeric."
         )
       }
 
@@ -92,11 +106,6 @@ polarize_assoc <- function(
       return(assoc)
 
       } else if (r_or_r2 == "r2") {
-        if (!is.numeric(value_1_eval)) {
-          stop(
-            "The R-squared approach requires an ordered outcome in the value_1 column."
-          )
-        }
 
       fmla <- as.formula(paste0(col1, "~", col2))
       assoc <- survey::svyglm(fmla, design = data)
@@ -131,10 +140,8 @@ polarize_assoc <- function(
       filter(.data[[weights]] != 0)
   }
 
-  # Creating survey design objects nested by the "by" argument
   nested_assocs <- input |>
     nest_by(across(any_of(by))) |>
-    # Using tidytable for speed
     tidytable::mutate(
       design_list = tidytable::map(
         data,
@@ -184,8 +191,7 @@ polarize_assoc <- function(
       )
     )
   }
-  # Removing columns with nested data, survey objects,
-  # and correlation matrices/summary.lm objects.
+  
   output <- select(
     output,
     -data, -design_list, -assoc_list

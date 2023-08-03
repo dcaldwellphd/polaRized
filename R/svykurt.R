5a3a4b483ed8ef9d4a651042f203f8d1c723640f
davidtcaldwell <dtcaldwell13@gmail.com>
2023-08-03 09:49:06 -0700

    Still tinkering with alternative svykurt function

--- R/svykurt.R
+++ R/svykurt.R
@@ -34,7 +34,7 @@ svykurt <- function(
     stop("design is not a survey design")
 
   # Storing variable in formula as string
-  # This is necessary to construct the svyciprop formula below
+  # This is necessary to construct the  moments formula below
   var_name <- as.character(x)[2]
 
   x <- model.frame(x, design$variables, na.action = na.pass)
@@ -62,8 +62,126 @@ svykurt <- function(
     as.formula(momnts_fmla),
     design,
     na.rm = na.rm
+  )
+
+centrl_momnts <- svycontrast(
+    momnts,
+    list(
+      mu4 = bquote(-3 * .(x^4) + 6 * .(x^2) * .(`I(x^2)`) - 4 * .(x) * .(`I(x^3)`) + .(`I(x^4)`)),
+      sigma2 = bquote(.(`I(x^2)`) - .(x^2))
+    )
+  )
+
+  return(centrl_momnts)
+
+}
+
+# This is the closest I've gotten to a working solution
+centrl_momnts <- svycontrast(
+    momnts,
+    list(
+      mu4 = bquote(-3 * .(x^4) + 6 * .(x^2) * .(`I(x^2)`) - 4 * .(x) * .(`I(x^3)`) + .(`I(x^4)`)),
+      sigma2 = bquote(.(`I(x^2)`) - .(x^2))
+    )
+  )
+
+centrl_momnts <- svycontrast(
+    momnts,
+    list(
+      mu4 = bquote(-3 * x^4 + 6 * x^2 * `I(x^2)` - 4 * x * `I(x^3)` + `I(x^4)`),
+      sigma2 = bquote(`I(x^2)` - x^2))
     )
 
+}
+
+
+  centrl_momnts <- svycontrast(
+    momnts,
+    list(
+      mu4 = eval(bquote(-3 * x^4 + 6 * x^2 * `I(x^2)` - 4 * x * `I(x^3)` + `I(x^4)`)),
+      sigma2 = eval(bquote(`I(x^2)` - x^2))
+    )
+  )
+
+}
+
+bquote(-3 * x^4 + 6 * x^2 * `I(x^2)` - 4 * x * `I(x^3)` + `I(x^4)`)
+
+  centrl_momnts <- svycontrast(
+    momnts,
+    list(
+      mu4 = eval(bquote(-3 * x^4 + 6 * x^2 * I(x^2) - 4 * x * I(x^3) + I(x^4))),
+      sigma2 = eval(bquote(I(x^2) - x^2))
+    )
+  )
+    
+
+  
+}
+
+eval(bquote(-3 * x^4 + 6 * x^2 * I(x^2) - 4 * x * I(x^3) + I(x^4)))
+
+  centrl_momnts <- svycontrast(
+    momnts,
+    list(mu4 = substitute(
+    -3 * x_4 + 6 * x_2 * ix_2 - 4 * x_1 * ix_3 + ix_4,
+    list(
+    x_4 = as.symbol(paste0(var_name, "^4")),
+    x_2 = as.symbol(paste0(var_name, "^2")),
+    ix_2 = as.symbol(paste0("I(", var_name, "^2)")),
+    x_1 = as.symbol(var_name),
+    ix_3 = as.symbol(paste0("I(", var_name, "^3)")),
+    ix_4 = as.symbol(paste0("I(", var_name, "^4)"))
+    )
+  ),
+    sigma2 = substitute(
+    ix_2 - x_2,
+    list(
+    x_2 = as.symbol(paste0(var_name, "^2")),
+    ix_2 = as.symbol(paste0("I(", var_name, "^2)")),
+    )
+  )
+  ))
+}
+
+substitute(
+    -3 * x_4 + 6 * x_2 * ix_2 - 4 * x_1 * ix_3 + ix_4,
+    list(
+      x_4 = as.symbol(paste0(var_name, "^4")),
+    x_2 = as.symbol(paste0(var_name, "^2")),
+    ix_2 = as.symbol(paste0("I(", var_name, "^2)")),
+    x_1 = as.symbol(var_name),
+    ix_3 = as.symbol(paste0("I(", var_name, "^3)")),
+    ix_4 = as.symbol(paste0("I(", var_name, "^4)"))
+    )
+  )
+
+substitute(
+      -3 * x_4 + 6 * x_2 * ix_2 - 4 * x * ix_3 + ix_4,
+      list(
+        x_4 = as.symbol(x^4),
+        x_2 = as.symbol(x^2),
+        ix_2 = as.symbol(I(x^2)),
+        ix_3 = as.symbol(I(x^3)),
+        ix_4 = as.symbol(I(x^4))
+      )
+    )
+
+  substitute(
+    ix_2 - x_2,
+    list(
+    x_2 = as.symbol(paste0(var_name, "^2")),
+    ix_2 = as.symbol(paste0("I(", var_name, "^2)")),
+    )
+  )
+
+}
+
+x_4 <- var_name^4
+x_2 <- x^2
+ix_2 <- I(x^2)
+ix_3 <- I(x^3)
+ix_4 <- I(x^4)
 
 
   mu4_fmla <- paste0(
@@ -90,18 +208,40 @@ svykurt <- function(
     "^2)"
   )
 
+  
 
   centrl_momnts <- svycontrast(
     momnts,
     list(
-      mu4_fmla,
-      sigma2_flma
+      mu4 = bquote(
+        -3 * .(as.name(x_4))
+}
+
+
+       -3 * .(as.name(x_4)) + 6 * .(as.name(x_2)) * .(as.name(ix_2)) - 4 * .(as.name(x)) * .(as.name(ix_3)) + .(as.name(ix_4))),
+      sigma2 =  bquote(.(as.name(ix_2)) - .(as.name(x_2)))
       )
     )
+
 
   return(centrl_momnts)
 
 }
+
+
+print(substitute(
+    -3 * x_4 + 6 * x_2 * ix_2 - 4 * x_1 * ix_3 + ix_4,
+    list(
+      x_4 = as.symbol(paste0(var_name, "^4")),
+    x_2 = as.symbol(paste0(var_name, "^2")),
+    ix_2 = as.symbol(paste0("I(", var_name, "^2)")),
+    x_1 = as.symbol(var_name),
+    ix_3 = as.symbol(paste0("I(", var_name, "^3)")),
+    ix_4 = as.symbol(paste0("I(", var_name, "^4)"))
+    )
+  ))
+
+
 
 print.svykurt <- function(x) {
   m <- as.matrix(x, ncol = 1)

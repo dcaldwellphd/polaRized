0e73cac167f970f6f1dd5b1a5835533d37051bfa
davidtcaldwell <dtcaldwell13@gmail.com>
2023-08-05 15:55:09 -0700

    Changed name of svyprop_extremism to svyextremism and added some better documentation

--- R/svyextremism.R
+++ R/svyextremism.R
@@ -1,26 +1,28 @@
-#' @title svyprop_extremism
+#' @title svyextremism
 #'
-#' @description Calculates proportion of extremism in complex survey data
+#' @description Calculates the proportion of extremism on ordered ratings scales in complex survey data.
 #'
-#' @param x A formula (e.g., ~var1) specifying the variable on which to estimate extremism.
+#' @param formula A formula (e.g., ~var) specifying the variable on which to estimate extremism.
 #' @param design The \code{survey.design} or \code{svyrep.design} object.
 #' @param na.rm Logical. Should cases with missing values be dropped?
-#' @param extreme_vals Optional values to use in the proportion calculation. The default (NULL) attempts to classify extremism based on scale length.
 #' @param method How to estimate confidence interval. See \code{\link[survey]{svyciprop}} for more details.
-#' @param level Confidence level for interval
+#' @param level Confidence level for interval.
 #' @param df Denominator degrees of freedom. See \code{\link[survey]{svyciprop}} for more details.
 #'
 #' @details
-#' This function is a wrapper around \code{survey::svyciprop}. It is designed to estimate the proportion of extreme responses to likert scale or feeling thermometer items. Extreme values can be specified by the user, or else the function will attempt to classify these based on scale length. If the scale has less than 10 (and more than 3) unique values, the function uses the minimum and maximum as extreme values. If the scale has 10 or 11 unique values, extreme values also include the second lowest and highest response categories. If the scale is a feeling thermometer with 100 or 101 unique response categories, the top 20 and bottom 20 unique values are classed as extreme. This approach is based on previous research into attitude polarization (Adams et al. 2012; Caldwell 2023; Cohen & Cohen 2021).
+#' This function wraps around the \code{svyciprop} function from the \code{survey} package. It is like calling \code{svyciprop(~I(variable == value), design), but \code{svyextremism} automatically classifies and measures the proportion of extreme values on ordered ratings scales with different lengths. If the scale has less than 10 (and more than 3) unique values, the function uses its minimum and maximum as extreme values. If the scale has 10 or 11 unique values, extreme values also include the second lowest and highest response categories. If the scale is a feeling thermometer with 100 or 101 unique response categories, the top 20 and bottom 20 unique values are classed as extreme. Calling \code{svyextremism} on any other scale length returns an error. The approach to measuring extremism implemented here is based on previous research into attitude polarization (Adams et al. 2012; Caldwell 2023; Cohen & Cohen 2021), and allows the \code{polarize_distr} function from this package to loop over heterogeneous rataing scales, calculating the proportion of extreme responses.
 #'
 #' @references
 #'
 #' Adams, J., Green, J., and Milazzo, C. (2012b). Has the British Public Depolarized Along With Political Elites? An American Perspective on British Public Opinion. \emph{Comparative Political Studies}, 45(4):507–530.
 #'
-#' #' Caldwell, D. (2023) \emph{Polarisation and Cultural Realignment in Britain, 2014-2019}. Doctoral thesis, Durham University.
+#' Caldwell, D. (2023) \emph{Polarisation and Cultural Realignment in Britain, 2014-2019}. Doctoral thesis, Durham University.
 #'
 #' Cohen, G. and Cohen, S. (2021). Depolarization, Repolarization and Redistributive Ideological Change in Britain, 1983–2016. \emph{British Journal of Political Science}, 51(3):1181–1202.
 #'
+#' @seealso [`svyciprop()`][survey::svyciprop]
+#' 
+#' @note This function was written without the knowledge or endorsement of the \code{survey} package authors.
 #'
 #' @return An object of class \code{svyciprop} giving the proportion and its confidence interval
 #'
@@ -29,19 +31,16 @@
 #' # Create survey design object
 #' toydesign <- svydesign(data = toydata_w, ids = ~1, weights = ~weight)
 #' # Print the proportion of extremism on a variable without specifying extreme values
-#' svyprop_extrmism(x = ~att_5, design = toydesign)
-#' # Print the proportion of extremism on a variable by specifying extreme values
-#' svyprop_extrmism(x = ~att_5, design = toydesign, extreme_vals = c(1, 5))
+#' svyextremism(~att_5, design = toydesign)
 #'
 #' @export
 #'
 #' @importFrom survey svyciprop
 
-svyprop_extrmism <- function(
-    x,
+svyextremism <- function(
+    formula,
     design,
     na.rm = FALSE,
-    extreme_vals = NULL,
     method = "logit",
     level = 0.95,
     df = degf(design)
@@ -52,9 +51,11 @@ svyprop_extrmism <- function(
 
   # Storing variable in formula as string
   # This is necessary to construct the svyciprop formula below
-  var_name <- as.character(x)[2]
+  var_name <- as.character(formula)[2]
 
-  x <- model.frame(x, design$variables, na.action = na.pass)
+  x <- model.frame(formula, design$variables, na.action = na.pass)
+  # Otherwise, length(unique(x)) returns 1,
+  # the number of variables in the formula
   x <- as.matrix(x)
 
   if (ncol(x) > 1)
@@ -64,29 +65,27 @@ svyprop_extrmism <- function(
     x <- x[!is.na(x)]
   }
 
-  if (!is.null(extreme_vals)) {
-    extremities <- extreme_vals
-  } else if (length(unique(x)) < 4) {
+  if (length(unique(x)) < 4) {
     stop("Scale length too short for meaningfully extreme values")
-  } else if (length(unique(x)) < 9) {
+  } else if (length(unique(x)) <= 9) {
     extremities <- c(
-      max(x, na.rm = TRUE),
-      min(x, na.rm = TRUE)
+      max(x, na.rm = na.rm),
+      min(x, na.rm = na.rm)
     )
     # Sometimes "10-point" scales include 0
-  } else if (length(unique(x)) %in% c(10, 11)) {
+  } else if (length(unique(x)) %in% c(10:11)) {
     extremities <- c(
       sort(unique(x), decreasing = TRUE)[1:2],
       sort(unique(x))[1:2]
     )
     # Sometimes feeling thermometers include 0
-  } else if (length(unique(x)) %in% c(100, 101)) {
+  } else if (length(unique(x)) %in% c(100:101)) {
     extremities <- c(
       sort(unique(x), decreasing = TRUE)[1:20],
       sort(unique(x))[1:20]
     )
   } else {
-    stop("Unusual scale length: specify extreme values manually")
+stop("Unusual scale length. The svvyextremism function is designed for ordered ratings scales, such as likert scales or feeling thermometers.")
   }
 
   fmla <- paste0(
